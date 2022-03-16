09dabee107b40dd1c780e1d0f95970e2d0cc9967
unknown <dasilvadeoliveirafabio@gmail.com>
2022-03-16 13:14:18 -0300

    teste de navbar 1

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,18 +10,18 @@ export default function NavBar() {
   return (
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">Logo</a>
-            <div onClick={()=>{h.push('/')}} style={{cursor:'pointer'}} class="nav-link active" aria-current="page">Home</div>
-            <button  class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"><PersistentDrawerLeft/></span>
-            </button>
-           
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Logo</a>
+                <div onClick={()=>{h.push('/')}} style={{cursor:'pointer'}} class="nav-link active" aria-current="page">Home</div>
+                <button  class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"><PersistentDrawerLeft/></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                
+                </div>
             </div>
-        </div>
-</nav>
+        </nav>
     </div>
   )
 }
--- src/components/NavBar/NavBar2.js
+++ src/components/NavBar/NavBar2.js
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import { useHistory } from 'react-router-dom'
+import PersistentDrawerLeft from '../PersistentDrawerLeft/PersistentDrawerLeft'
+import './NavBar.css'
 export default function NavBar2() {
+    const h = useHistory()
   return (
-    <div className='navbarBody'>NavBar2</div>
+    <div className='navbarBody'>
+        
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Logo</a>
+                <div onClick={()=>{h.push('/')}} style={{cursor:'pointer'}} class="nav-link active" aria-current="page">Home</div>
+                <button  class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"><PersistentDrawerLeft/></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                
+                </div>
+            </div>
+        </nav>
+    </div>
   )
 }
