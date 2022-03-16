fb22f973a2567682ac802f60ebc46952a3b5b216
unknown <dasilvadeoliveirafabio@gmail.com>
2022-03-16 13:26:52 -0300

    teste de navbar 3

--- src/components/NavBar/NavBar2.js
+++ src/components/NavBar/NavBar2.js
@@ -1,39 +1,39 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
-import PersistentDrawerLeft from '../PersistentDrawerLeft/PersistentDrawerLeft'
+//import { useHistory } from 'react-router-dom'
+//import PersistentDrawerLeft from '../PersistentDrawerLeft/PersistentDrawerLeft'
 import './NavBar.css'
 export default function NavBar2() {
-    const h = useHistory()
+   // const h = useHistory()
   return (
     <div className='navbarBody'>
         
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="f">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="f">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="f">Link</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="f" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="f">Action</a></li>
+            <li><a class="dropdown-item" href="f">Another action</a></li>
             <li class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="f">Something else here</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          
         </li>
       </ul>
       <form class="d-flex">
