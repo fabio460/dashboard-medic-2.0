58ae32dbe58de4b5b81f24fd16b10ec179349a50
unknown <dasilvadeoliveirafabio@gmail.com>
2022-03-16 15:10:07 -0300

    teste de navbar 7

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import combineRedurcer from './reducers/combineRedurcer';
+import './index.css'
 const store = createStore(combineRedurcer)
 ReactDOM.render(
   <Provider store={store}>
